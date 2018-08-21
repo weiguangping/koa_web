c60d25df1e2974d081e8286a1b241d78f49eb97a
darker <darker.wei@tianjingwo.com>
2018-08-21 17:31:31 +0800

    1

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            0,
+            "tab"
+        ],
+        "linebreak-style": [
+            0,
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
